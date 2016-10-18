85b71d394fb7c22c0c01ce5db98e6b3b52d086a6
Eric Hartman <imerichartman@gmail.com>
2016-10-18 13:23:24 -0400

    Exercise 3 complete

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
-puts "What is your name?"
-name = gets.chomp
-puts "Hi #{name}!"
+# puts "What is your name?"
+# name = gets.chomp
+# puts "Hi #{name}!"
+puts "How old are you?"
+age = gets.chomp
+puts "Did you know you were born in #{2016 - age.to_i}?"
